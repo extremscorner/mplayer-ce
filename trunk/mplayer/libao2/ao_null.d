7fb140f8f733d4821f2fb161602dd6cea213f269
agentx24 <agentx24@fb54ff3c-ddcf-11dd-bce9-bf72c0239d3a>
2009-04-05 22:23:08 +0000

    O_O

---
+++ trunk/mplayer/libao2/ao_null.d
@@ -0,0 +1,2 @@
+libao2/ao_null.d libao2/ao_null.o: libao2/ao_null.c config.h libaf/af_format.h config.h \
+  libao2/audio_out.h libao2/audio_out_internal.h
---
+++ trunk/mplayer/libao2/ao_pcm.d
@@ -0,0 +1,4 @@
+libao2/ao_pcm.d libao2/ao_pcm.o: libao2/ao_pcm.c config.h libavutil/common.h libavutil/mem.h \
+  libavutil/common.h mpbswap.h config.h libavutil/bswap.h config.h \
+  subopt-helper.h libaf/af_format.h config.h libaf/reorder_ch.h \
+  libao2/audio_out.h libao2/audio_out_internal.h mp_msg.h help_mp.h
