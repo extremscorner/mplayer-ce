bfe317d64e0ca02631be04142416b70fc1df3f56
metaradil <metaradil@fb54ff3c-ddcf-11dd-bce9-bf72c0239d3a>
2010-10-21 05:48:42 +0000

    Added paired single optimized AC-3 downmix.

--- trunk/mplayer/ffmpeg/libavcodec/ppc/dsputil_paired.c
+++ trunk/mplayer/ffmpeg/libavcodec/ppc/dsputil_paired.c
@@ -54,8 +54,51 @@ static void vorbis_inverse_coupling_paired(float *mag, float *ang, int blocksize
 	}
 }
 
+static void ac3_downmix_paired(float (*samples)[256], float (*matrix)[2], int out_ch, int in_ch, int len)
+{
+	const vector float zero = {0.0,0.0};
+	
+	vector float result[2];
+	vector float pair, coeffs;
+	
+	int i, c;
+	if (out_ch == 2) {
+		for (i=0; i<len*4-7; i+=8) {
+			result[0] = result[1] = zero;
+			
+			for(c=0; c<in_ch; c++) {
+				coeffs = psq_l(0,matrix[c],0,0);
+				pair = paired_lx(i, samples[c]);
+				result[0] = paired_madds0(pair, coeffs, result[0]);
+				result[1] = paired_madds1(pair, coeffs, result[1]);
+			}
+			
+			paired_stx(result[0], i, samples[0]);
+			paired_stx(result[1], i, samples[1]);
+		}
+	} else if (out_ch == 1) {
+		for(i=0; i<len*4-15; i+=16) {
+			result[0] = result[1] = zero;
+			
+			for(c=0; c<in_ch; c++) {
+				coeffs = psq_l(0,matrix[c],1,0);
+				pair = paired_lx(i, samples[c]);
+				result[0] = paired_madds0(pair, coeffs, result[0]);
+				pair = paired_lx(i+8, samples[c]);
+				result[1] = paired_madds0(pair, coeffs, result[1]);
+			}
+			
+			paired_stx(result[0], i, samples[0]);
+			paired_stx(result[1], i+8, samples[0]);
+		}
+	}
+}
+
 void dsputil_init_paired(DSPContext *c, AVCodecContext *avctx)
 {
 	if (CONFIG_VORBIS_DECODER)
 		c->vorbis_inverse_coupling = vorbis_inverse_coupling_paired;
+	
+	if (CONFIG_AC3_DECODER)
+		c->ac3_downmix = ac3_downmix_paired;
 }
