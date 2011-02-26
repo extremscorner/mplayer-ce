e75102624a24c09bcc1b374059a2dba93c4346c1
metaradil <metaradil@gmail.com>
2011-02-26 17:21:55 +0000

    Removed watchdog. Tidy up some more.

--- mplayer/osdep/plat_gekko.h
+++ mplayer/osdep/plat_gekko.h
@@ -3,34 +3,16 @@
 
 #ifdef GEKKO
 
-#include <sys/time.h>
-#include <sys/dir.h>
-#include <unistd.h>
-
 #include <gctypes.h>
-#include <ogc/mutex.h>
 
 extern bool reset_pressed;
 extern bool power_pressed;
-extern mutex_t watchdogmutex;
-extern int watchdogcounter;
 
 void DVDGekkoTick(bool silent);
 bool DVDGekkoMount();
 
-void plat_init (int *argc, char **argv[]);
-void plat_deinit (int rc);
-
-#define WATCH_TIMEOUT 5
-static inline void setwatchdogcounter(int counter) // -1 disable watchdog
-{
-	if(watchdogmutex==LWP_MUTEX_NULL) return;
-	LWP_MutexLock(watchdogmutex);
-	//printf("watchdogcounter: %i\n",counter);
-	watchdogcounter=counter;
-	LWP_MutexUnlock(watchdogmutex);
-}
-
+void plat_init(int *argc, char **argv[]);
+void plat_deinit(int rc);
 
 #endif
 
